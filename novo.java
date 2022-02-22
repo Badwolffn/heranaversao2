17dc9a6607d8d8db9e70c8537a88160f5f687428
Badwolffn <luckas.a.2016@gmail.com>
2022-02-22 19:46:35 +0000

    isso

---
+++ novo.java
@@ -0,0 +1,10 @@
+public class novo extends Imovel{
+
+    private double adicionalPreco(){
+        return adicionalPreco;
+
+    }
+    public void setAdiconalPreco(double adicionalPreco){
+        this.adicionalPreco = adicionalPreco;
+    }
+}
